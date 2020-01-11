ce003da9b7ab49e76cf8afd9a62a704eef579f6b
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2020-01-11 22:45:55 +0100

    EN version

---
+++ js/box.js
@@ -0,0 +1,19 @@
+var colourBox = document.getElementById('colour-box');
+var colours = ['#FB0E0E', '#1712F6', '#FFE713'];
+var counter = 0;
+
+function changeColour() {
+    if (counter >= colours.length){
+        counter = 0;
+    }
+
+    colourBox.style.background = colours[counter];
+    counter++;
+}
+
+var myTimer = setInterval(changeColour, 1000);
+
+colourBox.onclick = function () {
+    clearInterval(myTimer);
+    
+};
